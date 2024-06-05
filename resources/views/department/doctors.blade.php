d4727ab95c4a19743d2826d107b252f481e828f7
Shreyas Dhakal <dhakalshreyas2gmail.com>
2024-06-05 19:49:40 +0545

    doctors page

--- resources/views/department/doctors.blade.php
+++ resources/views/department/doctors.blade.php
@@ -1,29 +1,31 @@
 @extends('master')
 
 @section('content')
-    <section class="Doctors py-5">
-        <div class="container">
-            <h2 class="our-stories text-center pb-5">
-                Doctors in <span class="green-text">{{ $department->name }}</span>
-            </h2>
-            <div class="container text-center my-3">
-                <div class="row mx-auto my-auto">
-                    @foreach ($doctors as $doctor)
-                        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-                            <div class="card shadow-lg">
-                                <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                                    <div class="circle d-flex justify-content-center align-items-center">
-                                        <img class="doctor-image" src="{{ asset($doctor->image) }}" alt="{{ $doctor->name }}" />
-                                    </div>
-                                    <div class="doctor-name text-center mt-5">{{ $doctor->name }}</div>
-                                    <div class="doctor-designation text-center">{{ $doctor->designation }}</div>
-                                    <div class="doctor-nmc text-center">{{ $doctor->nmc_reg }}</div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
+<section class="doctors py-5">
+    <div class="container">
+        <h2 class="our-stories text-center pb-5">
+            Doctors in <span class="green-text">{{ $department->name }}</span>
+        </h2>
+        <div class="row">
+            @forelse ($doctors as $doctor)
+            <div class="col-lg-6 mt-4 mt-lg-0">
+                <div class="member d-flex align-items-start mt-5">
+                    <div class="pic">
+                        <img src="{{ asset($doctor->image) }}" class="img-fluid" alt="{{ $doctor->name }}" />
+                    </div>
+                    <div class="member-info">
+                        <h4>{{ $doctor->name }}</h4>
+                        <span>{{ $doctor->designation }}</span>
+                        <p>{{ $doctor->description }}</p>
+                    </div>
                 </div>
             </div>
+            @empty
+            <div class="col-12">
+                <p class="text-center">No doctors available.</p>
+            </div>
+            @endforelse
         </div>
-    </section>
+    </div>
+</section>
 @endsection
